9d419228c64b04b468ae4f1d7f2f35cc9f08fb06
136petrified <95456211+136petrified@users.noreply.github.com>
2025-04-19 19:02:49 -0700

    Update stk_sim.h

--- src/stk_sim.h
+++ src/stk_sim.h
@@ -92,61 +92,61 @@ double generate_value();
 
 // Stock
 /*  TYPE                    MEMBER                  */
-    struct Stock              * s_init(const char *name, const char *sym, const char *sector,
-                                    const double val, const int status, 
-                                    const unsigned avail_shares, const unsigned total_shares);
-    void                        s_refresh(struct Stock *stk_dest, const struct Stock *stk_src);
-    void                        s_update(struct Stock *stk_dest, const struct Stock *stk_src);
-    double                      s_calc_diff(const double prev, const double curr);
-    int                         s_bad_event();
-    int                         s_good_event();
-    struct Stock              * s_destroy(struct Stock *stk);
-    void                        s_listall(const struct Stock *stk);
+    struct Stock          * s_init(const char *name, const char *sym, const char *sector,
+                                   const double val, const int status, 
+                                   const unsigned avail_shares, const unsigned total_shares);
+    void                    s_refresh(struct Stock *stk_dest, const struct Stock *stk_src);
+    void                    s_update(struct Stock *stk_dest, const struct Stock *stk_src);
+    double                  s_calc_diff(const double prev, const double curr);
+    int                     s_bad_event();
+    int                     s_good_event();
+    struct Stock          * s_destroy(struct Stock *stk);
+    void                    s_listall(const struct Stock *stk);
 
 // Wallet
 /*  TYPE                    MEMBER                  */
-    struct Wallet             * w_init(const char *name, const int bal, struct Portfolio *pf, 
-                                    const int elligible_for_loan, struct Loan **loans);
-    void                        w_add_stock(struct Wallet *w, const struct Stock *stk, 
+    struct Wallet         * w_init(const char *name, const int bal, struct Portfolio *pf, 
+                                const int elligible_for_loan, struct Loan **loans);
+    void                    w_add_stock(struct Wallet *w, const struct Stock *stk, 
+                                        const unsigned shares);
+    void                    w_remove_stock(struct Wallet *w, const struct Stock *stk);
+    void                    w_update_stock(struct Wallet *w, const struct Stock *stk, 
                                             const unsigned shares);
-    void                        w_remove_stock(struct Wallet *w, const struct Stock *stk);
-    void                        w_update_stock(struct Wallet *w, const struct Stock *stk, 
-                                            const unsigned shares);
-    void                        w_buy(struct Wallet *w, const struct Market *m, const char *sym, 
-                                    const unsigned shares);
-    void                        w_sell(struct Wallet *w, const char *sym, const unsigned shares);
-    void                        w_sellall(struct Wallet *w, const char *sym);
-    void                        w_show_bal(struct Wallet *w);
-    inline double               w_get_bal(struct Wallet *w);
-    inline void                 w_set_bal(struct Wallet *w, const int val);
-    struct Wallet             * w_destroy(struct Wallet *w);
+    void                    w_buy(struct Wallet *w, const struct Market *m, const char *sym, 
+                                  const unsigned shares);
+    void                    w_sell(struct Wallet *w, const char *sym, const unsigned shares);
+    void                    w_sellall(struct Wallet *w, const char *sym);
+    void                    w_show_bal(struct Wallet *w);
+    inline double           w_get_bal(struct Wallet *w);
+    inline void             w_set_bal(struct Wallet *w, const int val);
+    struct Wallet         * w_destroy(struct Wallet *w);
 
 // Portfolio
 /*  TYPE                    MEMBER                  */
-    struct Portfolio          * pf_init();
-    void                        pf_add_stock(struct Portfolio *pf, const struct Stock *stk, 
-                                            const unsigned shares);
-    void                        pf_remove_stock(struct Portfolio *pf, const char *sym);
-    struct PortfolioStock     * pf_find_stock(const struct Portfolio *pf, const char *sym);
-    void                        pf_update_stock(struct Portfolio *pf, 
-                                                const struct PortfolioStock *ps_src);
-    struct Portfolio          * pf_destroy_stocks(struct Portfolio *pf);
-    struct Portfolio          * pf_destroy(struct Portfolio *pf);
+    struct Portfolio      * pf_init();
+    void                    pf_add_stock(struct Portfolio *pf, const struct Stock *stk, 
+                                         const unsigned shares);
+    void                    pf_remove_stock(struct Portfolio *pf, const char *sym);
+    struct PortfolioStock * pf_find_stock(const struct Portfolio *pf, const char *sym);
+    void                    pf_update_stock(struct Portfolio *pf, 
+                                            const struct PortfolioStock *ps_src);
+    struct Portfolio      * pf_destroy_stocks(struct Portfolio *pf);
+    struct Portfolio      * pf_destroy(struct Portfolio *pf);
 
 // PortfolioStock
 /*  TYPE                    MEMBER                  */
-    struct PortfolioStock     * ps_init(const char *name, const char *sym, const unsigned shares);
-    void                        ps_update_stock(struct PortfolioStock *ps_dest, 
-                                                const struct PortfolioStock *ps_src);
-    struct PortfolioStock     * ps_destroy(struct PortfolioStock *ps);
+    struct PortfolioStock * ps_init(const char *name, const char *sym, const unsigned shares);
+    void                    ps_update_stock(struct PortfolioStock *ps_dest, 
+                                            const struct PortfolioStock *ps_src);
+    struct PortfolioStock * ps_destroy(struct PortfolioStock *ps);
 
 // Loan
 /*  TYPE                    MEMBER                  */
-    struct Loan               * l_init(const unsigned amount, const unsigned fulfilled, 
-                                    const time_t deadline);
-    struct Loan               * l_destroy(struct Loan *l);
+    struct Loan           * l_init(const unsigned amount, const unsigned fulfilled, 
+                                   const time_t deadline);
+    struct Loan           * l_destroy(struct Loan *l);
 
 // General functions
-    void                        print_err_msg(FILE *fp, const char *msg);
+    void                    print_err_msg(FILE *fp, const char *msg);
 
 #endif // _STKSIM_H
