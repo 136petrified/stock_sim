7fe85c5bf1fd8f68401eb4fc497b3758fe27e141
136petrified <95456211+136petrified@users.noreply.github.com>
2025-04-24 14:48:17 -0700

    Define PFSTOCK_HASHTABLE

--- src/stk_sim_psht.h
+++ src/stk_sim_psht.h
@@ -1,3 +1,19 @@
 #ifndef STK_SIM_PFSTOCK_HASHTABLE_H
+#define STK_SIM_PFSTOCK_HASHTABLE_H
 
 #include "stk_sim.h"
+
+struct PFSTOCK_HASHTABLE {
+    struct Stock **stocks_buckets;
+    unsigned capacity;
+};
+
+struct PFSTOCK_HASHTABLE * PFSTOCK_HASHTABLE_init(const unsigned capacity);
+unsigned PFSTOCK_HASHTABLE_hash(const char *stk_sym);
+struct PFSTOCK_HASHTABLE * PFSTOCK_HASHTABLE_insert(struct PFSTOCK_HASHTABLE *table, struct PortfolioStock *ps);
+struct PFSTOCK_HASHTABLE * PFSTOCK_HASHTABLE_remove(struct PFSTOCK_HASHTABLE *table, const char *sym);
+struct PortfolioStock * STOCK_HASHTABLE_find(struct PFSTOCK_HASHTABLE *table, const char *sym);
+struct PFSTOCK_HASHTABLE * STOCK_HASHTABLE_destroy(struct PFSTOCK_HASHTABLE *table);
+
+
+#endif
