9b600778fad952f3ae1abd466018a9faf833de8b
136petrified <95456211+136petrified@users.noreply.github.com>
2025-04-29 12:17:13 -0700

    Reformat

--- src/stk_sim_psht.h
+++ src/stk_sim_psht.h
@@ -4,16 +4,18 @@
 #include "stk_sim.h"
 
 struct PFSTOCK_HASHTABLE {
-    struct PFStockList **pfs_buckets;
-    unsigned capacity;
+/*  TYPE                        MEMBER                  */
+    struct PFStockList *        pfs_buckets;
+    unsigned                    capacity;
 };
 
-struct PFSTOCK_HASHTABLE * PFSTOCK_HASHTABLE_init(const unsigned capacity);
-unsigned PFSTOCK_HASHTABLE_hash(const char *stk_sym);
-void PFSTOCK_HASHTABLE_insert(struct PFSTOCK_HASHTABLE *table, struct PortfolioStock *ps);
-void PFSTOCK_HASHTABLE_remove(struct PFSTOCK_HASHTABLE *table, const char *sym);
-struct PortfolioStock * STOCK_HASHTABLE_find(struct PFSTOCK_HASHTABLE *table, const char *sym);
-struct PFSTOCK_HASHTABLE * STOCK_HASHTABLE_destroy(struct PFSTOCK_HASHTABLE *table);
+/*  TYPE                        MEMBER                  */
+    struct PFSTOCK_HASHTABLE *  PFSTOCK_HASHTABLE_init(const unsigned capacity);
+    unsigned                    PFSTOCK_HASHTABLE_hash(const char *stk_sym);
+    void                        PFSTOCK_HASHTABLE_insert(struct PFSTOCK_HASHTABLE *table, struct PortfolioStock *ps);
+    void                        PFSTOCK_HASHTABLE_remove(struct PFSTOCK_HASHTABLE *table, const char *sym);
+    struct PortfolioStock *     STOCK_HASHTABLE_find(struct PFSTOCK_HASHTABLE *table, const char *sym);
+    struct PFSTOCK_HASHTABLE *  STOCK_HASHTABLE_destroy(struct PFSTOCK_HASHTABLE *table);
 
 
 #endif
--- src/stk_sim_stkht.c
+++ src/stk_sim_stkht.c
@@ -1,22 +1,10 @@
 #include "stk_sim_stkht.h"
 
 struct STOCK_HASHTABLE * STOCK_HASHTABLE_init(const unsigned capacity) {
-    struct STOCK_HASHTABLE *table = (struct STOCK_HASHTABLE *) malloc(sizeof(struct STOCK_HASHTABLE));
-    table->stocks_buckets = (struct STOCK_HASHTABLE *) malloc(capacity * sizeof(struct Stock));
-    table->capacity = capacity;
-
-    for (unsigned i = 0; i < capacity; ++i) {
-        table->stocks_buckets = NULL;
-    }
-
-    return table;
 }
 
 struct STOCK_HASHTABLE * STOCK_HASHTABLE_insert(struct STOCK_HASHTABLE *table, struct Stock *stk) {
-    unsigned bucket = STOCK_HASHTABLE_hash(stk->sym);
-    stk->next = table->stocks_buckets[bucket];
-    table->stocks_buckets[bucket] = stk;
-    return table;
+
 }
 
 struct STOCK_HASHTABLE * STOCK_HASHTABLE_remove(struct STOCK_HASHTABLE *table, const char *sym) {
