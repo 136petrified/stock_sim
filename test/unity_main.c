1cce0139da3c5e3dc1ee43c88cb5158bfc714056
136petrified <95456211+136petrified@users.noreply.github.com>
2025-05-08 21:58:10 -0700

    Update unity_main.c

--- test/unity_main.c
+++ test/unity_main.c
@@ -13,6 +13,16 @@ void test_mkt_init(void) {
     TEST_ASSERT_TRUE(0);
 }
 
+void test_mpm(void) {
+    struct MarketParam *mpm = mpm_init("MyMarket", 100, 0, 0, 10, 1, NULL);
+    
+    TEST_ASSERT_EQUAL(0, strcmp("MyMarket", mpm->name));
+    TEST_ASSERT_EQUAL(100, mpm->max_stocks);
+    TEST_ASSERT_EQUAL(true, mpm->is_open);
+
+    mpm = mpm_destroy(mpm);
+}
+
 void test_gen_print_err(void) {
     print_err("./err_test.txt", "Error occurred! Test message.");
 }
@@ -29,6 +39,7 @@ void test_gen_strdup(void) {
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_mkt_init);
+    RUN_TEST(test_mpm);
     RUN_TEST(test_gen_print_err);
     RUN_TEST(test_gen_strdup);
     return UNITY_END();
